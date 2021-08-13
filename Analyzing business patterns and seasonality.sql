e6a3432cf4a463bf6671078918735842915c3790
xidan chen <xidan.chen@gmail.com>
2021-08-13 10:39:31 +0800

    Update Analyzing business patterns and seasonality.sql

--- Analyzing business patterns and seasonality.sql
+++ Analyzing business patterns and seasonality.sql
@@ -31,11 +31,34 @@ GROUP BY YEAR(wp.created_at), WEEK(wp.created_at)
 /*We’re considering adding live chat support to the website to improve our customer experience. 
 Could you analyze the average website session volume, by hour of day and by day week, so that we can staff appropriately? 
 Let’s avoid the holiday time period and use a date range of 
-Sep 15 - Nov 15, 2013. */
+Sep 15 - Nov 15, 2012. */
+DROP TABLE IF EXISTS sessions_only;
+CREATE TEMPORARY TABLE sessions_only
+SELECT
+DATE(created_at),
+HOUR(created_at) AS hours,
+WEEKDAY(created_at) AS weekdays,
+COUNT(website_session_id) AS sessions
+FROM website_sessions
+WHERE created_at BETWEEN '2012-09-15' AND '2012-11-15'
+GROUP BY DATE(created_at), HOUR(created_at), WEEKDAY(created_at)
+;
 
 
+SELECT
+hours,
 
+ROUND(AVG(CASE WHEN weekdays = 0 THEN sessions ELSE NULL END), 1) AS Monday,
+ROUND(AVG(CASE WHEN weekdays = 1 THEN sessions ELSE NULL END), 1) AS Tuesday,
+ROUND(AVG(CASE WHEN weekdays = 2 THEN sessions ELSE NULL END), 1) AS Wednesday,
+ROUND(AVG(CASE WHEN weekdays = 3 THEN sessions ELSE NULL END), 1) AS Thursday,
+ROUND(AVG(CASE WHEN weekdays = 4 THEN sessions ELSE NULL END), 1) AS Friday,
+ROUND(AVG(CASE WHEN weekdays = 5 THEN sessions ELSE NULL END), 1) AS Saturday,
+ROUND(AVG(CASE WHEN weekdays = 6 THEN sessions ELSE NULL END), 1) AS Sunday
 
+FROM sessions_only
+GROUP BY hours
+;
 
 
 
